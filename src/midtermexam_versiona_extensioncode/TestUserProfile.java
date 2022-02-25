a92970501b48a2ace2411ce5384b9f99870bb523
vinz angeles <angelevi@sheridancollege.ca>
2022-02-25 09:57:01 -0500

    Wih main method now

---
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -0,0 +1,19 @@
+
+package midtermexam_versiona_extensioncode;
+
+import java.util.Scanner;
+
+
+public class TestUserProfile {
+    
+    public static void main(String[] args){
+        
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter your Name: ");
+        String name = s.next();
+        
+        System.out.println("Input you genre: ");
+        String genre = s.next();
+    }
+}
